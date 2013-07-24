0c58349c4cdd3283208a05bd53219e0a87bc28b2
Isaac Elias <nicecurelom@gmail.com>
2013-07-24 08:47:12 -0700

    painting and putting in images works

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
     var color = $('#color').val();
     box.css('background-color', color);
 
-    $('#colors').prepend(box);
+    box.appendTo('#color_divs')
 
     $('#color').val('').focus();
   };
@@ -26,7 +26,32 @@ $(document).ready(function(){
     paint_box.css('background-color', color);
   };
 
+  var add_img = function(){
+    var input_string = $("#img_url").val();
+    var box = $('<div>');
+    box.addClass('box');
+
+          box.css('background-image',  "url(" + input_string + ")");
+          box.css('background-size', "50px");
+          box.appendTo("#image_divs");
+          $("#canvas").css('background-image', "url(" + input_string + ")");
+          $("#canvas").css('background-size', "400px 125px");
+          $("#img_url").val('');
+
+  };
+
+  var set_image = function(){
+    var box = $(this);
+    var img_url = box.css('background-image');
+    
+    $("#canvas").css('background-image', img_url);
+    $("#canvas").css('background-size', "400px 125px"); 
+
+  }
+
   $('#add_color').on('click', add_color);
-  $('#colors').on('click', '.box', set_color);
+  $('#color_divs').on('click', '.box', set_color);
   $('.paint_box').on('mouseover', paint);
+  $('#add_img').on('click', add_img);
+  $('#image_divs').on('click', '.box', set_image);
 });
